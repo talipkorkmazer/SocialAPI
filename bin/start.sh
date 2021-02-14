688736de26607fafae65bbeb527b0bac4d8e50f4
Talip Can Korkmazer <t.korkmazer@emakina.com.tr>
2021-02-14 13:50:28 +0300

    Start sh remove uncessary services

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd laradock
 
-docker-compose up -d nginx mysql phpmyadmin redis redis-webui mongo mongo-webui elasticsearch php-worker workspace "$@"
+docker-compose up -d nginx mysql phpmyadmin redis workspace "$@"
